7f261618e473f9474f048640a2872a01a6e560b7
Kevin Holder <kevholderx@gmail.com>
2021-04-08 18:51:29 -0400

    verify

---
+++ verify/DeriveBTCPool-args.js
@@ -0,0 +1,15 @@
+module.exports = [
+  [
+    "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c",
+    "0xfce146bf3146100cfe5db4129cf6c82b0ef4ad8c",
+    "0x19e0E8413DEe3AfFd94bdd42519d01935a0CF0c2",
+  ],
+  [18, 8, 18],
+  "Derive BTCB/renBTC/oBTC",
+  "deriveBTC",
+  200,
+  4000000,
+  0,
+  0,
+  "0xBF30578733e0e5E12c8016554d6Ef3968ADBEe12",
+]
---
+++ verify/DeriveUSDPoolLPToken-args.js
@@ -0,0 +1,1 @@
+module.exports = ["Derive DAI/USDC/USDT/OUSD", "deriveUSD", 18]
